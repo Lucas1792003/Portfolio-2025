0b02086378215230ba0c11f927be399237e11127
Wai Yan <waiyanpaing183@gmail.com>
2025-08-21 13:19:43 +0700

    Initial

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'/portfolio2025/',
+  base:'/Portfolio-2025/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
